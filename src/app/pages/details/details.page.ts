face55df5574a0469af171b19841affc7376c284
Austine <austine.amah@gmail.com>
2022-08-29 21:50:51 +0100

    Chart Js

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { budgets } from 'src/app/services/shared/budget';
+import { chartData } from 'src/app/services/shared/chartData';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-details',
@@ -10,14 +12,35 @@ import { budgets } from 'src/app/services/shared/budget';
 export class DetailsPage implements OnInit {
   budget: any;
   id: any;
+  chart: any;
+  delayed: any;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
     this.getBudget();
+    this.createChart();
   }
 
   getBudget() {
     this.budget = budgets.find((b) => b.id == this.id);
+  }
+
+  createChart() {
+    this.chart = new Chart('burnChart', {
+      type: 'bar',
+
+      data: {
+        ...chartData.data,
+        datasets: [
+          {
+            ...chartData.data.datasets[0],
+            backgroundColor: this.budget?.color?.active,
+          },
+        ],
+      },
+      options: chartData.options,
+    });
   }
 }
